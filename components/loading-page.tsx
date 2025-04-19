0d50a1fab3b8198e62922f2a6845727ea4382573
Zeyad Alagamy <wezza2003@gmail.com>
2025-04-19 04:12:56 +0200

    feat: add LoadingPage component with InfiniteCircleProgress for loading state

---
+++ components/loading-page.tsx
@@ -0,0 +1,7 @@
+import { InfiniteCircleProgress } from "@/components/ui/infinite-circle-progress";
+
+export const LoadingPage = () => (
+  <div className="flex h-[100svh] w-full items-center justify-center">
+    <InfiniteCircleProgress />
+  </div>
+);
