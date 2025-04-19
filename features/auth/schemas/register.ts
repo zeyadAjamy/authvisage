0d425a63f8248e6abcc918c20db52cc027a67d57
Zeyad Alagamy <wezza2003@gmail.com>
2025-04-19 04:14:19 +0200

    feat: add login and registration schemas using Zod for form validation

---
+++ features/auth/schemas/register.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+export const schema = z
+  .object({
+    fullname: z.string().min(3, { message: "Name is too short" }),
+    email: z.string().email({
+      message: "Invalid email address",
+    }),
+    password: z
+      .string()
+      .min(8, { message: "Password is too short" })
+      .refine(
+        (value) => {
+          return (
+            value.match(/[a-z]/) &&
+            value.match(/[A-Z]/) &&
+            value.match(/[0-9]/) &&
+            value.match(/!|@|#|\$|%|\^|&|\*|\(|\)|-|\+|_/)
+          );
+        },
+        {
+          message:
+            "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+        },
+      ),
+    confirm_password: z.string().min(8, { message: "Password is too short" }),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Passwords do not match",
+  });
+
+export type RegistrationFormSchema = z.infer<typeof schema>;
