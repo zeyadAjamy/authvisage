63293259ed0995a594679fb8f680e5b6459fb9be
Zeyad Alagamy <wezza2003@gmail.com>
2025-05-19 21:30:21 +0300

    feat: enhance LoginPage with success and error callbacks for authentication

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,16 +1,30 @@
+"use client";
+
 import { LoginForm } from "@/features/auth/components/login-form";
 import { AuthStream } from "@/features/auth/components/auth-stream";
+import { client } from "@/lib/supabase/config";
+import { Session } from "@supabase/supabase-js";
+import { toast } from "react-toastify";
 
-const LoginPage = () => (
-  <div className="flex min-h-screen flex-col items-center justify-center">
-    <div className="grid grid-cols-1 items-center gap-10 p-10 md:grid-cols-2 md:p-0">
-      <AuthStream
-        streamPurpose="login"
-        isOAuth={false}
-      />
-      <LoginForm />
+const LoginPage = () => {
+  const successCallback = async (session: Session) => {
+    client.auth.setSession(session);
+  };
+  const errorCallback = () => {
+    toast.error("Authentication failed");
+  };
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="grid grid-cols-1 items-center gap-10 p-10 md:grid-cols-2 md:p-0">
+        <AuthStream
+          streamPurpose="login"
+          isOAuth={false}
+          onSuccess={successCallback}
+          onError={errorCallback}
+        />
+        <LoginForm />
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default LoginPage;
