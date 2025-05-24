32424d8da82564b6e5aeab902708adef70c8bc44
Zeyad Alagamy <wezza2003@gmail.com>
2025-05-25 02:01:04 +0300

    feat: remove ProjectProvider wrapper from ProjectsPage for simplified structure

--- app/(dashboard)/projects/page.tsx
+++ app/(dashboard)/projects/page.tsx
@@ -1,14 +1,11 @@
 import { ProjectList } from "@/features/projects/components/project-list";
 import { ProjectHeader } from "@/features/projects/components/project-header";
-import { ProjectProvider } from "@/features/projects/contexts/project";
 
 const ProjectsPage = () => (
-  <ProjectProvider>
-    <div className="flex flex-col gap-6">
-      <ProjectHeader />
-      <ProjectList />
-    </div>
-  </ProjectProvider>
+  <div className="flex flex-col gap-6">
+    <ProjectHeader />
+    <ProjectList />
+  </div>
 );
 
 export default ProjectsPage;
