127413757e5b2af13f854c7ae3ea1b3233f306ea
Zeyad Alagamy <wezza2003@gmail.com>
2025-05-25 11:07:14 +0300

    feat: add CI configuration for Lighthouse CI and initial Lighthouse settings

---
+++ lighthouserc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ci: {
+    upload: {
+      target: "lhci",
+      serverBaseUrl: "http://localhost:60440/",
+      token: "67626825-075b-4f44-8eb4-908dff2d9178", // NOT A SECRET
+    },
+  },
+};
