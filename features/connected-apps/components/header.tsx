917476178c40a33d06c370c7678cfd2bb3ab40b7
Zeyad Alagamy <wezza2003@gmail.com>
2025-05-25 03:31:59 +0300

    fix: adjust padding in ConnectedAppsHeader and ProjectFormHeader components for consistency

--- features/connected-apps/components/header.tsx
+++ features/connected-apps/components/header.tsx
@@ -5,7 +5,7 @@ import { ConnectedAppsFilters } from "./filters";
 
 export const ConnectedAppsHeader = () => {
   return (
-    <div className="bg-card flex flex-col items-start gap-4 rounded-lg border p-18 px-6 pb-16 md:items-end xl:flex-row">
+    <div className="bg-card flex flex-col items-start gap-4 rounded-lg border p-10 px-6 md:items-end xl:flex-row">
       <div className="flex w-full flex-col overflow-hidden rounded-md">
         <h1 className="text-2xl font-bold md:text-3xl">Connected Apps</h1>
         <p className="text-muted-foreground text-lg">
--- features/projects/components/project-form-header.tsx
+++ features/projects/components/project-form-header.tsx
@@ -11,7 +11,7 @@ export const ProjectFormHeader = ({
   title,
   description,
 }: ProjectFormHeaderProps) => (
-  <div className="bg-card flex flex-col items-end justify-between gap-4 rounded-lg border p-18 px-6 pb-16 md:flex-row">
+  <div className="bg-card flex flex-col items-end justify-between gap-4 rounded-lg border p-10 px-6 md:flex-row">
     <div className="flex flex-col">
       <h1 className="text-2xl font-bold md:text-3xl">{title}</h1>
       <p className="text-muted-foreground">{description}</p>
